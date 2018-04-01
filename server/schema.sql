0751fca4f232004c7671c7c5f4c7de01cc9d3281
inh556 <yanbin.jin@gmail.com>
2018-04-01 10:19:17 -0700

    database implement

--- server/schema.sql
+++ server/schema.sql
@@ -4,9 +4,28 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(200)  NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
+
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username  varchar(20)   NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
+
 
 
 
